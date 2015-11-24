e0c237285c1b37d0d8bc2b9ce0168ec5fc05e64f
CarlHembrough <carlhuk@gmail.com>
2015-11-24 11:18:13 +0000

    Added run script.

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+base_url="http://localhost:8080"
+
+if [ ! -z "$1" ]
+  then
+    base_url="$1"
+fi
+
+if [ ! -z "$2" ]
+  then
+    export BROWSERSTACK_URL="$2"
+fi
+
+export BABBAGE_URL=$base_url
+
+echo Running tests against: ${base_url}
+
+mvn clean test
+
+exit $? # return the code from the last command.
