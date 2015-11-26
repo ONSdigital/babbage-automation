5ea9845e63742f362b9b2d9c2a2094753fa21ea0
CarlHembrough <carlhuk@gmail.com>
2015-11-26 14:23:02 +0000

    Extend browser test base to set driver instance.

--- src/test/java/com/github/onsdigital/babbage/test/NavigationTest.java
+++ src/test/java/com/github/onsdigital/babbage/test/NavigationTest.java
@@ -1,12 +1,13 @@
 package com.github.onsdigital.babbage.test;
 
+import com.github.onsdigital.babbage.test.base.BrowserTestBase;
 import com.github.onsdigital.babbage.test.page.HomePage;
 import org.junit.Test;
 
 /**
  * Tests to browse around the website to test the general site health and navigation.
  */
-public class NavigationTest {
+public class NavigationTest extends BrowserTestBase {
 
     @Test
     public void navigate() {
